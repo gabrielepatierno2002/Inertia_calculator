27fbb9628e646bc423044aced7b0b828c7d89852
gabrielepatierno2002 <gabriele.patierno2002@gmail.com>
2026-02-26 15:07:25 +0100

    versione definitiva

--- config_inertia.m
+++ config_inertia.m
@@ -5,10 +5,10 @@
 % -------------------------------------------------------------------------
 
 %% --- Dati razzo COMPLETO (con motore) [da OpenRocket] -----------------
-M_total  = 23.05;              % [kg] massa totale razzo con motore
-M_rocket_with_no_motor = 14.751 ;
-cg_total = 1.70;              % [m] baricentro totale (asse Z dal nose tip)
-I_total  = diag([10.24, 10.24, 0.06]); % [kg*m^2] tensore inerzia totale @ cg_total (3x3)
+L_rocket = 2.63;                        % [m] lunghezza totale razzo (origine O naso)
+M_total  = 23.05;                       % [kg] massa totale razzo con motore
+cg_total = 1.70;                        % [m] baricentro totale (asse Z dal nose tip)
+I_total  = diag([10.84, 10.84, 0.066]); % [kg*m^2] tensore inerzia totale @ cg_total (3x3)
 
 
 %% --- Baricentro (coerenti con il sistema di riferimento) -----------------
@@ -17,10 +17,8 @@ cg_noMotor    = 1.38;         % [m] baricentro razzo senza motore (asse Z)
 
 %% --- Motore (cilindro cavo) ----------------------------------------------
 M_motor_dry = 3.456;           % [kg] massa casing motore
-M_motor_with_prop = 8.273;      % [kg] massa motore casing+prop
+M_motor_with_prop = 8.273;     % [kg] massa motore casing+prop
 R_out       = 0.098;           % [m] raggio esterno motore
 R_in        = 0.096;           % [m] raggio interno motore (cilindro cavo)
 L_motor     = 0.702;           % [m] lunghezza motore
 
-%% --- Geometria razzo ------------------------------------------------------
-L_rocket = 2.63;               % [m] lunghezza totale razzo (origine O naso)
